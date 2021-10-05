1831ff61e3c9802b261a2132c29f5cae686ec646
ArturArakelyan0411 <artur.araqelyan.2004@gmail.com>
2021-10-05 18:39:26 +0400

    added columns section scroll on mouse dragging

--- src/components/Columns/index.jsx
+++ src/components/Columns/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import {useSelector, useDispatch} from "react-redux";
 
 import Column from "../Column";
@@ -14,16 +14,62 @@ const Columns = () => {
   const columns = useSelector(state => state.columnsReducer.columns);
 
   const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+  const [coordinates, setCoordinates] = useState({
+    clientX: 0,
+    scrollLeft: 0
+  });
+
+  const scroller = useRef(null);
+  const scrollerContent = useRef(null);
+
+  const handleMouseDown = (e) => {
+    const {scrollLeft} = scroller.current;
+
+    if(e.target === scroller.current || e.target === scrollerContent.current) {
+      setIsScrolling(true);
+      setCoordinates({
+        clientX: e.clientX,
+        scrollLeft
+      });
+    }
+  }
+
+  const handleMouseMove = (e) => {
+    const {clientX, scrollLeft} = coordinates;
+
+    if(isScrolling && scroller && scroller.current) {
+      scroller.current.scrollLeft = scrollLeft - clientX + e.clientX;
+    }
+  }
 
   const modalOpen = (column, card) => {
     dispatch(editingColumnAction(column, card));
     setModalIsOpen(true);
   }
 
+  useEffect(() => {
+    function stopScrolling() {
+      setIsScrolling(false);
+      setCoordinates({
+        clientX: 0,
+        scrollLeft: 0
+      });
+    }
+
+    window.addEventListener('mouseup', stopScrolling);
+    return () => window.removeEventListener('mouseup', stopScrolling);
+  }, []);
+
   return (
     <>
-      <div className={styles.home__columns}>
-        <div className={styles.home__columns_content}>
+      <div
+        className={styles.home__columns}
+        ref={scroller}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+      >
+        <div className={styles.home__columns_content} ref={scrollerContent}>
           {columns.map(column => {
             return (
               <Column
